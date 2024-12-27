7e6a9b2009e33c350aadf50619e41134b6dba37b
Sarita <126271555+SaritaPhD@users.noreply.github.com>
2024-12-27 12:04:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_community.llms import Ollama
-import os
+import os 
 from logger import logging
 from exception import CustomException
 from dotenv import load_dotenv
